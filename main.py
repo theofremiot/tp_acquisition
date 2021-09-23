3708b4002cd0a00d7a33be8f30611d8aa7b84feb
theofremiot <theofremiot@gmail.com>
2021-09-23 18:17:40 +0200

    ajout de la fonction pour enregistrer une image quand on appuie sur c

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
+import time
 
 cap = cv2.VideoCapture(0)
+
+idx = 0
 
 while True:
     ret, frame = cap.read()  # 1 frame acquise à chaque iteration
@@ -10,5 +13,9 @@ while True:
     key = cv2.waitKey(1)  # on évalue la touche pressée
     if key & 0xFF == ord('q'):  # si appui sur 'q'
         break  # sortie de la boucle while
+    elif key & 0xFF == ord('c'):
+        cv2.imwrite(f"img_{idx}.jpg", frame)
+        idx += 1
+        time.sleep(2)
 cap.release()
 cv2.destroyAllWindows()
