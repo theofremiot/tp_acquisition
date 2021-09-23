50728e9fdf9f590a315d5f163b056e3254024371
theofremiot <theofremiot@gmail.com>
2021-09-23 18:22:39 +0200

    modification de la partie pour enregistrer l'image quand on appuie sur c

--- main.py
+++ main.py
@@ -11,11 +11,14 @@ while True:
     ret, frame = cap.read()  # 1 frame acquise à chaque iteration
     cv2.imshow('Capture_Video', frame)  # affichage
     key = cv2.waitKey(1)  # on évalue la touche pressée
+
     if key & 0xFF == ord('q'):  # si appui sur 'q'
         break  # sortie de la boucle while
-    elif key & 0xFF == ord('c'):
-        cv2.imwrite(f"img_{idx}.jpg", frame)
-        idx += 1
-        time.sleep(2)
+
+    elif key & 0xFF == ord('c'):  # si appuie sur 'c'
+        cv2.imwrite(f"img_{idx}.jpg", frame)  # on enregistre l'image avec un idx pour en faire plusieurs
+        idx += 1  # on incremente l'index
+        time.sleep(2)  # on met une petite pause parce que sinon cela prend plusieurs image si on appuie trop
+
 cap.release()
 cv2.destroyAllWindows()
