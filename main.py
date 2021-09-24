ba2ab0c1f338dc04c94e462f9782192b7191bb89
theofremiot <theofremiot@gmail.com>
2021-09-24 12:39:12 +0200

    affichage en niveau de gris de l'histogramme

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ idx = 0
 
 while True:
     ret, frame = cap.read()  # 1 frame acquise à chaque iteration
+
+    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+
     cv2.imshow('Capture_Video', frame)  # affichage
     key = cv2.waitKey(1)  # on évalue la touche pressée
 
@@ -18,7 +21,19 @@ while True:
     elif key & 0xFF == ord('c'):  # si appuie sur 'c'
         cv2.imwrite(f"img_{idx}.jpg", frame)  # on enregistre l'image avec un idx pour en faire plusieurs
         idx += 1  # on incremente l'index
-        time.sleep(2)  # on met une petite pause parce que sinon cela prend plusieurs image si on appuie trop
+        time.sleep(0.5)  # on met une petite pause parce que sinon cela prend plusieurs image si on appuie trop
+
+    channels = [0]
+    mask = None
+    histSize = [256]
+    ranges = [0, 256]
+    hist = cv2.calcHist(frame, channels, mask, histSize, ranges)
+
+    plt.plot(hist)  # où hist est la sortie de cv2.calcHist
+    plt.title('Histogramme')
+    plt.draw()  # execute l'affichage
+    plt.pause(0.0001)  # delai nécessaire a l'affichage
+    plt.cla()  # évite la superposition des courbes
 
 cap.release()
 cv2.destroyAllWindows()
